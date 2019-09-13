e5debfb5464665d9d826470783e38e5356e5fde5
Christian Thomas <info@webgyver.de>
2019-09-13 21:17:19 +0200

    log

--- kiosk/run.js
+++ kiosk/run.js
@@ -21,11 +21,17 @@ const isFullscreen = !!size;
 
 (function run() {
   carlo.launch(options).then(app => {
-    app.on("exit", () => run());
+    app.on("exit", () => {
+      console.log('kiosk exits');
+      run();
+    });
 
     setTimeout(() => {
       app.evaluate(url => (location.href = url), `http://localhost:${port}`);
       if (!size) app.mainWindow().fullscreen();
     }, 1000);
-  }, run);
+  }, () => {
+    console.log('kiosk dies');
+    run();
+  }));
 })();
